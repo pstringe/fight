37082d029f2f4459414f58946c7260ef2660727e
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-28 13:24:27 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import '@fontsource/roboto';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="app">
+      {/*Header*/}
+      {/*Opponents*/}
+      {/*Swipe Tools*/}
+    </Box>
   );
 }
 
