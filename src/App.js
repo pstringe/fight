e335f84ca220046fb48a67323c3513ba69ad4898
Poitier Stringer <stringerpoitier@gmail.com>
2021-04-28 13:44:25 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Header from './Header';
 import '@fontsource/roboto';
 
 function App() {
   return (
     <Box className="app">
-      {/*Header*/}
+      <Header/>
       {/*Opponents*/}
       {/*Swipe Tools*/}
     </Box>
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,34 @@
+import PersonIcon from '@material-ui/icons/Person';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { 
+    Typography,
+    IconButton,
+    Box 
+} from "@material-ui/core"
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    }
+});
+
 const Header = () => {
-    return (  );
+    const classes = useStyles();
+
+    return ( 
+        <Box className={classes.root}>
+            <IconButton>
+                <PersonIcon fontSize='large' />
+            </IconButton>
+            <Typography variant='h5'>FIGHT</Typography>
+            <IconButton>
+                <ChatBubbleIcon fontSize='large' />
+            </IconButton>
+        </Box>
+     );
 }
  
 export default Header;
